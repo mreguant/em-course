dc0217d34ceb63606a1b8bc8a1a3d6e16c26a054
mreguant <40800513+mreguant@users.noreply.github.com>
2021-08-27 06:19:04 -0500

    adding main pages, still very preliminary

---
+++ day1.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ec2c5947-3b0b-4214-8c56-06c5cea7eee9
+
+
+# ╔═╡ ef70b7d4-0719-11ec-290f-9539c06bdd7e
+
+
+# ╔═╡ Cell order:
+# ╠═ec2c5947-3b0b-4214-8c56-06c5cea7eee9
+# ╠═ef70b7d4-0719-11ec-290f-9539c06bdd7e
